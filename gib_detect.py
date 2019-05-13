43c93ed7954a6e8e0b63ecae46430805c9de3e20
Christian Doll <doll@cs.uni-bonn.de>
2019-05-13 14:46:56 +0200

    bumped to python3

--- gib_detect.py
+++ gib_detect.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import pickle
 import gib_detect_train
@@ -6,7 +6,7 @@ import gib_detect_train
 model_data = pickle.load(open('gib_model.pki', 'rb'))
 
 while True:
-    l = raw_input()
+    line = input()
     model_mat = model_data['mat']
     threshold = model_data['thresh']
-    print gib_detect_train.avg_transition_prob(l, model_mat) > threshold
+    print(gib_detect_train.avg_transition_prob(line, model_mat) > threshold)
